3b2b1ceb097e5a132ec87c1c14e1dba7a58d2835
nokotan <kyoshizaki@jcom.home.ne.jp>
2016-11-15 17:24:55 +0900

    ストーリー画面、操作説明画面のフェードイン、フェードアウトの追加

--- QGJ2016GasshukuGroup3/SousaSetsumei.cpp
+++ QGJ2016GasshukuGroup3/SousaSetsumei.cpp
@@ -9,6 +9,7 @@ static int StoryBackImage = -1;
 static bool SetsumeiInitialized = false;
 static int FontHandle = -1;
 static int Phase = 0;
+// Flames < 0: フェードイン中, Flames > 0: フェードアウト中, Flames == 0: それ以外
 static int Flames = 0;
 static bool SpacePressed = false;
 
@@ -19,52 +20,108 @@ static enum InternalState {
 } InternalCurrentState;
 
 InternalState StoryScene() {
-	int PressedTime = getKey(KEY_INPUT_SPACE);
+	if (Flames < 0) {
+		Flames--;
 
-	if (PressedTime > 120) {
-		SpacePressed = false;
-		return InternalState::SceneEnded;
-	} else if (PressedTime > 0) {
-		SpacePressed = true;
-	} else if (SpacePressed) {
-		SpacePressed = false;
+		if (Flames <= -64) {
+			Flames = 0;
+		}
+	} else if (Flames > 0) {
+		Flames++;
 
-		++Phase;
-
-		if (Phase >= 10) {
+		if (Flames >= 64) {
+			Flames = -1;
 			Phase = 1;
 			return InternalState::SetsumeiPhase;
+		}
+	} 
+	
+	if (Flames <= 0) {
+		int PressedTime = getKey(KEY_INPUT_SPACE);
+
+		if (PressedTime > 120) {
+			SpacePressed = false;
+			return InternalState::SceneEnded;
+		} else if (PressedTime > 0) {
+			SpacePressed = true;
+		} else if (SpacePressed) {
+			SpacePressed = false;
+
+			++Phase;
+
+			if (Phase >= 10) {
+				Phase = 9;
+				Flames = 1;
+			}
 		}
 	}
 
 	DrawGraph(0, 0, StoryBackImage, FALSE);
 	DrawGraph(27, 394, StoryImages[Phase], FALSE);
 
+	if (Flames != 0) {
+		if (Flames > 0) {
+			SetDrawBlendMode(DX_BLENDMODE_ALPHA, Flames * 4);
+		} else {
+			SetDrawBlendMode(DX_BLENDMODE_ALPHA, 255 + Flames * 4);
+		}
+
+		DrawBox(0, 0, 640, 480, GetColor(0, 0, 0), TRUE);
+		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+	}
+
 	return InternalState::StoryPhase;
 }
 
 InternalState SetsumeiScene() {
-	Flames++;
+	if (Flames < 0) {
+		Flames--;
 
-	if (Phase == 0) {
-		if (getKeyPress(KEY_INPUT_SPACE, PRESS_ONCE)) {
-			Phase = 1;
+		if (Flames <= -64) {
+			Flames = 0;
 		}
+	}
+	else if (Flames > 0) {
+		Flames++;
 
-		DrawGraph(0, 0, SetsumeiImage[0], FALSE);
-	} else {
-		if (getKeyPress(KEY_INPUT_SPACE, PRESS_ONCE)) {
+		if (Flames >= 64) {
+			Flames = 0;
+			Phase = 1;
 			return InternalState::SceneEnded;
 		}
+	} else {
+		if (getKeyPress(KEY_INPUT_SPACE, PRESS_ONCE)) {
+			if (Phase == 0) {
+				Phase = 1;
+			} else {
+				Flames = 1;
+			}
+		}
+	}
 
+	if (Phase == 0) {
+		DrawGraph(0, 0, SetsumeiImage[0], FALSE);
+	} else {
 		DrawGraph(0, 0, SetsumeiImage[1], FALSE);
 	}
 
-	int Brightness = Flames % 256 <= 128 ? Flames % 256 : 128 - (Flames - 128) % 256;
-	SetDrawBlendMode(DX_BLENDMODE_ALPHA, Brightness);
-	DrawStringToHandle(300 - 3, 425 - 3, "[Space] で次に進む...", GetColor(0, 0, 156), FontHandle);
-	DrawStringToHandle(300, 425, "[Space] で次に進む...", GetColor(96, 96, 255), FontHandle);
-	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+	//int Brightness = Flames % 256 <= 128 ? Flames % 256 : 128 - (Flames - 128) % 256;
+	//SetDrawBlendMode(DX_BLENDMODE_ALPHA, Brightness);
+	//DrawStringToHandle(300 - 3, 425 - 3, "[Space] で次に進む...", GetColor(0, 0, 156), FontHandle);
+	//DrawStringToHandle(300, 425, "[Space] で次に進む...", GetColor(96, 96, 255), FontHandle);
+	//SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+
+	if (Flames != 0) {
+		if (Flames > 0) {
+			SetDrawBlendMode(DX_BLENDMODE_ALPHA, Flames * 4);
+		}
+		else {
+			SetDrawBlendMode(DX_BLENDMODE_ALPHA, 255 + Flames * 4);
+		}
+
+		DrawBox(0, 0, 640, 480, GetColor(0, 0, 0), TRUE);
+		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+	}
 
 	return InternalState::SetsumeiPhase;
 }
@@ -72,7 +129,7 @@ InternalState SetsumeiScene() {
 STATE setsumei() {
 	if (!SetsumeiInitialized) {
 		Phase = 0;
-		Flames = 0;
+		Flames = -1;
 		InternalCurrentState = InternalState::StoryPhase;
 
 		// 画像などがまだ読み込まれていない時に一度だけ初期化
@@ -102,6 +159,7 @@ STATE setsumei() {
 		case SceneEnded:
 			// 終了処理
 			SetsumeiInitialized = false;
+			// タイトルのBGMを停止
 			StopSoundMem(GetHandle("Sound1"));
 			return GAME;
 	}
